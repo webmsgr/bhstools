52bcbd1c4f71b30301cb25291e3681a5c075417b
Wackery <32279125+webmsgr@users.noreply.github.com>
2021-04-22 09:19:06 -0700

    Update main.js

--- main.js
+++ main.js
@@ -22,13 +22,11 @@ var x = setInterval(function () {
     document.getElementById("countdown").innerHTML = "School Over!";
     return;
   }
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  // Time calculations for hours, minutes and seconds
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML =
-    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  document.getElementById("countdown").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
 }, 1000);
