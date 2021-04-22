66ac566e278947fea048bd7607ef2a5468e881e5
Wackery <32279125+webmsgr@users.noreply.github.com>
2021-04-22 10:15:57 -0700

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,21 @@ function getEndOfSchool() {
   end.setSeconds(0);
   return end;
 }
+function getTimeUntilNextPeriod() {
+  now = new Date()
+  endtimes = ["9:5","10:40","12:30","14:5"]
+  ends = []
+  for (i = 0; i < endtimes.length; i++) {
+    today = new Date()
+    time = endtimes[i].split(":")
+    today.setHours(parseInt(time[0]))
+    today.setMinutes(parseInt(time[1]))
+    today.setSeconds(0)
+    ends.push(today)   
+  }
+  console.log(ends)
+  return -1
+}
 // code stolen lmao
 function CountdownEnd() {
   // Get today's date and time
@@ -32,7 +47,18 @@ function CountdownEnd() {
   $("#countdown-end").html(hours + "h " + minutes + "m " + seconds + "s ")
 }
 function CountdownPeriod() {
-$("#countdown-period").html("TODO")
+  var distance = getTimeUntilNextPeriod()
+  if (distance < 0) {
+    $("#countdown-period").html("0h 0m 0s ")
+    return;
+  }
+  // Time calculations for hours, minutes and seconds
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  $("#countdown-period").html(hours + "h " + minutes + "m " + seconds + "s ")
 }
 function onSecond() {
   // function handling all countdowns
