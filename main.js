c61b0b6cb6b294f20bcf98b7ffbc3c2c9663df4e
Wackery <32279125+webmsgr@users.noreply.github.com>
2021-04-22 09:08:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ $( document ).ready(function () {
     // set up links 
     $("a").attr("target","_blank").attr("referrerpolicy","no-referrer")
 })
+
+function getEndOfSchool() {
+    end = new Date()
+    end.setHours(14)
+    end.setMinutes(5)
+    end.setSeconds(0)
+    return end
+}
