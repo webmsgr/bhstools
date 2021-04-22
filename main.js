9b34600ee39639329c6902d1a327027e4b7737cf
Wackery <32279125+webmsgr@users.noreply.github.com>
2021-04-22 09:13:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,28 @@ function getEndOfSchool() {
     end.setSeconds(0)
     return end
 }
+// code stolen lmao
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = getEndOfSchool() - now;
+  if (distance < 0) {
+    document.getElementById("countdown").innerHTML = "School Over!";
+      return
+  }
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  
+  
+}, 1000);
