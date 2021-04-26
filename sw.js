1a731b0d662204045c4f2c97af11331092b44e84
Wackery <32279125+webmsgr@users.noreply.github.com>
2021-04-26 12:23:50 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,34 @@
 // its serviceworker time
+var CACHE_NAME = 'bhstools-v1';
+var urlsToCache = [
+  '/',
+  '/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+
+  var cacheAllowlist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
