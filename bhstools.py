e63d25a56630619e7a4956faec08b9aeec998a58
Wackery <zapcontact000@gmail.com>
2021-05-20 17:30:03 +0000

    gitpod url detection

--- bhstools.py
+++ bhstools.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, abort
 import os
 import urllib.parse
 import requests
 import xmltodict
-hostname = "bhstools.herokuspp.com" # maybe add gitpod detection to change this url automaticly?
+if os.environ.get("GITPOD_WORKSPACE_URL",None) is not None:
+    hostname = os.environ.get("GITPOD_WORKSPACE_URL").replace("https://","5000-")
+else:
+    hostname = "bhstools.herokuspp.com" 
 service = urllib.parse.quote_plus("https://{}/ticket".format(hostname))
 loginurl = "https://sso.bethelsd.org/login?service={}".format(service)
 validateurl = "https://sso.bethelsd.org/serviceValidate?service={}&ticket={}".format(service,"{}")
