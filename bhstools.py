8b3725bcabe44d1eac6984e3aec09433f08f64c8
Restyled.io <commits@restyled.io>
2021-05-20 18:54:48 +0000

    Restyled by autopep8

--- bhstools.py
+++ bhstools.py
@@ -4,23 +4,31 @@ import urllib.parse
 import requests
 import xmltodict
 import json
-if os.environ.get("GITPOD_WORKSPACE_URL",None) is not None:
-    hostname = os.environ.get("GITPOD_WORKSPACE_URL").replace("https://","5000-")
+if os.environ.get("GITPOD_WORKSPACE_URL", None) is not None:
+    hostname = os.environ.get(
+        "GITPOD_WORKSPACE_URL").replace("https://", "5000-")
 else:
-    hostname = "bhstools.herokuspp.com" 
+    hostname = "bhstools.herokuspp.com"
 service = urllib.parse.quote_plus("https://{}/ticket".format(hostname))
 loginurl = "https://sso.bethelsd.org/login?service={}".format(service)
-validateurl = "https://sso.bethelsd.org/serviceValidate?service={}&ticket={}".format(service,"{}")
+validateurl = "https://sso.bethelsd.org/serviceValidate?service={}&ticket={}".format(
+    service, "{}")
 app = Flask(__name__)
+
+
 @app.route("/")
 def homepage():
     return render_template("index.html")
+
+
 @app.route("/login")
 def login():
     return redirect(loginurl)
+
+
 @app.route("/ticket")
 def ticketlogin():
-    ticket = request.args.get("ticket",None)
+    ticket = request.args.get("ticket", None)
     if ticket is None:
         abort(400)
     nformat = validateurl.format(ticket)
@@ -32,9 +40,13 @@ def ticketlogin():
     userdata = resp["cas:authenticationSuccess"]
     studentid = userdata["cas:user"]
     print(studentid)
-    return redirect("https://{}/".format(hostname)) #why
+    return redirect("https://{}/".format(hostname))  # why
+
+
 @app.route("/brewCoffee")
 def brewCoffee():
     abort(418)
+
+
 if __name__ == "__main__":
-    app.run(port=os.environ.get("PORT",8080))
+    app.run(port=os.environ.get("PORT", 8080))
