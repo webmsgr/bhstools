d9d39112457eb619402fd03badeb3cdc819dbf40
Wackery <32279125+webmsgr@users.noreply.github.com>
2021-05-20 10:20:57 -0700

    Update bhstools.py

--- bhstools.py
+++ bhstools.py
@@ -3,7 +3,8 @@ import os
 import urllib.parse
 import requests
 import xmltodict
-service = urllib.parse.quote_plus("https://bhstools.herokuapp.com/ticket")
+hostname = "bhstools.herokuspp.com" # maybe add gitpod detection to change this url automaticly?
+service = urllib.parse.quote_plus("https://{}/ticket".format(hostname))
 loginurl = "https://sso.bethelsd.org/login?service={}".format(service)
 validateurl = "https://sso.bethelsd.org/serviceValidate?service={}&ticket={}".format(service,"{}")
 app = Flask(__name__)
